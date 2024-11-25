ecc815263e23d53b4609b1310a83b8462bea9d4c
talhamazhar11 <subhanmazhar789@gmail.com>
2024-11-26 01:43:47 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Show Popup
+function showPopup(id) {
+    document.getElementById(id).style.display = 'flex';
+}
+
+// Close Popup
+function closePopup(id) {
+    document.getElementById(id).style.display = 'none';
+}
