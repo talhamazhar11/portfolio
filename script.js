4eb2801087fe8de8372320b56bfde638a6a7fc47
talhamazhar11 <subhanmazhar789@gmail.com>
2024-11-26 01:50:45 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Show Popup
-function showPopup(id) {
-    document.getElementById(id).style.display = 'flex';
-}
-
-// Close Popup
-function closePopup(id) {
-    document.getElementById(id).style.display = 'none';
-}
